001b85cb15f6b06898be1d8ed48b4662a412958e
Matt Clarke <matt.clarke@esss.se>
2021-02-17 06:56:11 +0100

    Day 19 trying a new direction

--- day_19/day_19.py
+++ day_19/day_19.py
@@ -156,116 +156,13 @@ def solve(incoming, target):
             # print("Deadend 2")
             return
         for r in CHOICES[elem]:
-            if index > 200:
-                print(index, r)
+            if index >= 224:
+                print(index, r, "".join(so_far))
+                # print("".join(target))
+                # raise Exception("Oops")
             # print("current", elem, r)
             expand = break_down_formula(r)
             prefix = so_far[:index]
             suffix = so_far[index + 1:]
             if expand[0] == target[index]:
                 _solve(prefix + expand + suffix, index + 1)
-
-            # Evolve the first element
-            # TODO: stop this recusing off forever
-            # TODO: Should we always do this even if the current elem matches the target?
-
-            # SPIKE: ignore Ca => CaF style for now as need to work out how to handle those
-            # And P => BP
-            # if expand == prev:
-            #     continue
-            #
-            # if len(prefix + expand + suffix) < len(target):
-            #     _solve(prefix + expand + suffix, index, expand)
-            # else:
-            #     return
-
-
-    _solve(incoming, 0, [])
-
-
-for x in REPLACEMENTS["e"]:
-    print(x)
-    solve(break_down_formula(x), break_down_formula(target))
-
-
-
-# def find_where_diverge(a, b):
-#     index = 0
-#     for i, j in zip(a, b):
-#         if i != j:
-#             if i.islower() or j.islower():
-#                 index -= 1
-#             break
-#         index += 1
-#     return index
-#
-#
-# assert find_where_diverge("CF", "CF") == 2
-# assert find_where_diverge("CFG", "CFG") == 3
-# assert find_where_diverge("CFG", "CFX") == 2
-# assert find_where_diverge("A", "B") == 0
-# assert find_where_diverge("ACa", "ACb") == 1
-# assert find_where_diverge("Ca", "Cb") == 0
-# assert find_where_diverge("C", "") == 0
-# assert find_where_diverge("", "A") == 0
-#
-# possibles = {}
-#
-#
-# def find_poss(molecule, seen):
-#     if molecule not in REPLACEMENTS:
-#         return
-#
-#     for poss in REPLACEMENTS[molecule]:
-#         front = poss[0:2] if len(poss) > 1 and poss[1].islower() else poss[0]
-#         if front in seen:
-#             continue
-#         seen.add(front)
-#         find_poss(front, seen)
-#
-#
-# for k in REPLACEMENTS:
-#     poss = set()
-#     find_poss(k, poss)
-#     possibles[k] = poss
-#
-#
-# def solve(current: str, target: str):
-#     best = [10000000000000]
-#     dead_ends = set()
-#
-#     def _solve(current, target, count, depth=0):
-#         if len(current) > len(target) or count >= best[0]:
-#             return False
-#         if current == target:
-#             print("SOLVED", count, depth)
-#             best[0] = count
-#             return True
-#
-#         front = get_front(current)
-#         front_t = get_front(target)
-#         if front not in REPLACEMENTS:
-#             return False
-#         for option in REPLACEMENTS[front]:
-#             new_molecule = option + current[len(front):]
-#
-#             index = find_where_diverge(new_molecule, target)
-#             if index > 0:
-#                 print(depth, count + 1, len(new_molecule[index:]))
-#                 res = _solve(new_molecule[index:], target[index:], count+1, depth + 1)
-#                 # print("OUT")
-#
-#             # If there is no way to get to the target's first molecule then skip
-#             front_opt = get_front(new_molecule)
-#             if front_opt not in possibles or front_t not in possibles[front_opt]:
-#                 continue
-#
-#             # print(new_molecule)
-#             _solve(new_molecule, target, count + 1)
-#
-#     def get_front(current):
-#         return current[0:2] if len(current) > 1 and current[1].islower() else current[0]
-#
-#     _solve(current, target, 0)
-#
-# solve("e", target)
